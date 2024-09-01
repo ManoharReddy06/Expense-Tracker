670770601deffeebfbbfe206e2504b777f731772
ManoharReddy06 <anreddymanoharreddy123@gmail.com>
2024-09-01 18:04:58 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import InputBox from './components/inputbox';
+
+function App() {
+  return (
+    <div className="container">
+      <InputBox />
+    </div>
+  );
+}
+
+export default App;
